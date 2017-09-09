8f230341d37238d88dcd26adb2d90abcc3ee107f
Zachary Champion <zchampion270@gmail.com>
2017-09-09 10:54:40 -0600

    Made sharing.clj slightly more readable.

--- Adam/sharing.clj
+++ Adam/sharing.clj
@@ -7,6 +7,7 @@
 ; Date Last Updated:  08 September 2017
 
 (defn -main [& args]
-   (repeatedly 1 (partial shuffle ["Hi", "Hello", "Howdy", "Greetings", "Hey", "G'day", "Good day",
+   (repeatedly 1
+     (partial shuffle ["Hi", "Hello", "Howdy", "Greetings", "Hey", "G'day", "Good day",
                                    "How are you", "What's up", "How goes it", "How do you do", "Hi there"])))
 (-main)
