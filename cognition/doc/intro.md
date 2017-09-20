c30502e1022e40942727a157a424a18994549456
Zachary Champion <zchampion270@gmail.com>
2017-09-20 15:50:53 -0600

    Started on a matching function

---
+++ cognition/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cognition
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cognition/test/cognition/core_test.clj
@@ -0,0 +1,7 @@
+(ns cognition.core-test
+  (:require [clojure.test :refer :all]
+            [cognition.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
