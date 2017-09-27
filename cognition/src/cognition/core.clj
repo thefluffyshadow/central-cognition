362a3eec4810f561a2339da66ee7caa074ff813e
Zachary Champion <zchampion270@gmail.com>
2017-09-27 14:02:13 -0600

    .

--- cognition/src/cognition/core.clj
+++ cognition/src/cognition/core.clj
@@ -1,21 +1,6 @@
 (ns cognition.core
   (:gen-class))
 
-(defn pair-name
-  "Returns the name of the pair"
-  [pair]
-  (first pair))
-
-(defn pair-filler
-  "Returns the filler of the pair"
-  [pair]
-  (rest pair))
-
-(defn match
-  "determine whether the pattern matches the constant given the current binding form and return updated binding form"
-  [pattern constant binding]
-  binding)
-
 (defn bullshit []
   (print "bullshit"))
 
